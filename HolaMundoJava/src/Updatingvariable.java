256637ca338846c5595b79e82be54a98ed66c591
juanachar <juanachar@gmail.com>
2022-08-12 10:54:57 -0300

    first commit

---
+++ HolaMundoJava/src/Updatingvariable.java
@@ -0,0 +1,9 @@
+public class Updatingvariable {
+     public static void main(String[] args){
+
+          var salary=1000;
+          System.out.println(salary);
+
+     }
+}
+0
---
+++ HolaMundoJava/src/Variable.java
@@ -0,0 +1,9 @@
+public class Variable {
+    public static void main(String[] args){
+
+        int speed=10;
+        System.out.println(speed);
+        String name="juan achar";
+        System.out.println(name);
+    }
+}
